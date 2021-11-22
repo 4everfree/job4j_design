09326d91cc3295e6cf2071322ba3bc98ba14755a
4everfree <32147862+4everfree@users.noreply.github.com>
2021-11-22 17:45:29 +0100

    Update BackwardArrayIt.java

--- src/main/java/ru/job4j/it/BackwardArrayIt.java
+++ src/main/java/ru/job4j/it/BackwardArrayIt.java
@@ -5,7 +5,7 @@ import java.util.NoSuchElementException;
 
 public class BackwardArrayIt implements Iterator<Integer> {
     private final int[] data;
-    private int point = 0;
+    private int point = data.length;
 
     public BackwardArrayIt(int[] data) {
         this.data = data;
@@ -21,6 +21,6 @@ public class BackwardArrayIt implements Iterator<Integer> {
         if (!hasNext()) {
             throw new NoSuchElementException();
         }
-        return data[data.length - ++point];
+        return data[++point];
     }
 }
